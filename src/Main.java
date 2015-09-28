fc40d9d7b389dc38ae5d2ec9252e4b4c20c7d9bd
Eneko <enekodlt@gmail.com>
2015-09-28 13:29:55 +0200

    Corregido

--- src/Main.java
+++ src/Main.java
@@ -25,8 +25,11 @@ import org.jsoup.select.Elements;
 
 public class Main {
 	
+	//Yo le indico la carpeta en la que quiero que lo guarde.
+	//Quítalo si quieres y el archivo aparecerá en la carpeta del proyecto.
 	private static String carpetaFicheros = "/home/eneko/Documentos/";
-	//El sufijo es -page-2, así que para meterlo en un bucle quitamos el número y probamos hasta que no devuelva nada
+	
+	//El sufijo es -page2, así que dejamos el String listo para añadirle un parámetro con el número de página
 	private static String sufijoPaginacion= "-page%s";
 	private static long inicio;
 
@@ -34,9 +37,9 @@ public class Main {
 		inicio = System.currentTimeMillis();
 	   String ruta = "http://www.esprit.es/chaquetas-abrigos-hombre";
 	   String ficheroOferta = "Abrigos.txt";
-	   int codigo = getStatusConnectionCode(ruta);
-	   System.out.println("Código de la web : "+codigo);
 	   
+	   //Comprobar el código que devuelve la petición a la URL que le pasamos
+	   int codigo = getStatusConnectionCode(ruta);
 	   if(codigo==200){
 		   crearOComprobarArchivo(ruta, carpetaFicheros+ficheroOferta);
 	   }else{
@@ -80,6 +83,7 @@ public class Main {
 	        				"Nuevos abrigos", JOptionPane.INFORMATION_MESSAGE, 1, null, botones, botones[0]);
 	        		if(respuesta == 0){
 	        			//Se pueden abrir pestañas de firefox de 2 en 2. Hay que añadir un espacio entre dos URL
+	        			//Si abro más de 2 a la vez, abre una nueva ventana con todas las pestañas
 	            		try {
 	            			for(int x=0;x<diferenciaArtículos.size();x++){
 	            				String dirAbrigo=diferenciaArtículos.get(x)[1].trim();
@@ -97,7 +101,8 @@ public class Main {
 
 	        		}
 	        	}else{
-	    			JOptionPane.showMessageDialog(new JFrame(), "No hay nuevos abrigos");
+	        		long fin = System.currentTimeMillis() - inicio;
+	    			JOptionPane.showMessageDialog(new JFrame(), fin+" No hay nuevos abrigos");
 	    		}
 	        }
 	}
@@ -107,41 +112,37 @@ public class Main {
 	    documentoProcesadoAnterior="";
         documentoAGuardar = "";
         int x=1;
-	    try {
-	    	String url;
-	    	Boolean iguales =false;
-	    	do{
-	    		documentoProcesado = "";
-	    		if(x>1){
-	    			url = String.format(stringUrl+sufijoPaginacion,x);
-	    		}else{
-	    			url = stringUrl;
-	    		}
-	    		System.out.println("Muestro: "+url);
-	    		
-		        Document doc = getHtmlDocument(url);
-		        //Extraigo en otro documento solo el div con la lista
-		        Element primerDiv = doc.getElementById("styleoverview");
-		      
-		        Elements articulos = primerDiv.getElementsByClass("style");
-		        for (Element articulo : articulos){
-		        	//El selector span:nth-child(x) busca al padre de span y elige al elemento hijo en la posición x
-		        	documentoProcesado += "\n"+articulo.select("p.style-name span:nth-child(2)").text() + " -- "+articulo.getElementsByTag("a").attr("href");
-		        }
-		        System.out.println("pagina "+x);
-			    if(!documentoProcesado.equals(documentoProcesadoAnterior)){
-			    	System.out.println("Hay diferencias");
-			    	documentoProcesadoAnterior = documentoProcesado;
-			    	documentoAGuardar += documentoProcesado;
-			        x++;
-		        }else{
-		        	System.out.println("Son iguales");
-		        	iguales=true;
-		        }
-	    	}while(!iguales);
-		} finally {
-	       
-		} 
+    	String url;
+    	Boolean iguales =false;
+    	/*
+    	 * Esta web, devuelve contenido repetido si intentamos leer un indice de página que no existe.
+    	 * Si solo hay 2 páginas e intentamos leer la 3, nos volverá a enseñar la 2.
+    	 * Leemos hasta que se repita el contenido
+    	 */
+    	do{
+    		documentoProcesado = "";
+    		if(x>1){
+    			url = String.format(stringUrl+sufijoPaginacion,x);
+    		}else{
+    			url = stringUrl;
+    		}	    		
+	        Document doc = getHtmlDocument(url);
+	        //Extraer elemento con el div contiene todos los artículos
+	        Element primerDiv = doc.getElementById("styleoverview");
+	        //Extraer todos los div que contienen los datos sobre cada artículo 
+	        Elements articulos = primerDiv.getElementsByClass("style");
+	        for (Element articulo : articulos){
+	        	//El selector span:nth-child(x) busca al padre de span y elige al elemento hijo en la posición x
+	        	documentoProcesado += "\n"+articulo.select("p.style-name a span:nth-child(2)").text() + " -- "+articulo.getElementsByTag("a").attr("href");
+	        }
+		    if(!documentoProcesado.equals(documentoProcesadoAnterior)){
+		    	documentoProcesadoAnterior = documentoProcesado;
+		    	documentoAGuardar += documentoProcesado;
+		        x++;
+	        }else{
+	        	iguales=true;
+	        }
+    	}while(!iguales);
 	    return documentoAGuardar;
 	}
 	
@@ -151,7 +152,6 @@ public class Main {
 		try{
 			escritor = new FileWriter(archivo);
 			escritor.write(codHtml);
-			
 			escribeBien = true;
 		}catch(FileNotFoundException e){
 			System.out.println("No existe el fichero o la carpeta");
@@ -168,7 +168,6 @@ public class Main {
 				try {
 					escritor.close();
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
@@ -181,37 +180,25 @@ public class Main {
 		BufferedReader lector = null;
 		ArrayList<String[]> diferencia = new ArrayList<String []>();		
 		try{
+			BufferedReader reader = new BufferedReader(new StringReader(codHtml));
 			archivo = new FileReader(fil);
 			lector = new BufferedReader(archivo);
-			String linea, codArchivo;
-			codArchivo = "";
-			while ((linea = lector.readLine()) != null){
-				codArchivo += "\n"+linea;
-			}
-
-			BufferedReader reader = new BufferedReader(new StringReader(codHtml));
-			BufferedReader reader2;
-			
 			String linea1, linea2;
 			Boolean repetida;
-			while ((linea1 = reader.readLine()) != null){
+			while ((linea1 = lector.readLine()) != null){
 				repetida = false;
-				reader2 = new BufferedReader(new StringReader(codArchivo));
-				while((linea2 = reader2.readLine()) != null){
-					if(linea1.equals(linea2)){
-						repetida = true;
-						break;
-					}
-				}
-				if(!repetida){
+				linea2=reader.readLine();
+				System.out.println(linea1);
+				System.out.println(linea2);
+				if(!linea1.equals(linea2)){
 					String[] datos= linea1.split("--");
 					diferencia.add(datos);
-				}
-			}
+				}	
+			}		
 		}catch(FileNotFoundException e){
-			
+			e.getMessage();
 		}catch (IOException e) {
-			
+			e.getMessage();
 		}finally{
 			if (lector != null){
 				try {
@@ -241,9 +228,9 @@ public class Main {
 	    Response response = null;
 		
 	    try {
-		response = Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(100000).ignoreHttpErrors(true).execute();
+	    	response = Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(100000).ignoreHttpErrors(true).execute();
 	    } catch (IOException ex) {
-		System.out.println("Excepción al obtener el Status Code: " + ex.getMessage());
+	    	System.out.println("Excepción al obtener el Status Code: " + ex.getMessage());
 	    }
 	    return response.statusCode();
 	}
@@ -259,9 +246,9 @@ public class Main {
 	    Document doc = null;
 		try {
 		    doc = Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(100000).get();
-		    } catch (IOException ex) {
+		} catch (IOException ex) {
 			System.out.println("Excepción al obtener el HTML de la página" + ex.getMessage());
-		    }
+		}
 	    return doc;
 	}
 }
